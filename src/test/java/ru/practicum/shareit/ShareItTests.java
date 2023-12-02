9f0c18ddb1dce7ceb984b3f1bcd3e32713c48041
IgorVasilev <777igorvasiliev@gmail.com>
2023-12-02 20:49:29 +0300

    fix: checkstyle

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,13 +3,11 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
 @SpringBootTest
 class ShareItTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
